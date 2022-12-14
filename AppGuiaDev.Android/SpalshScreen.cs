67aefdac3d63dedab6a05760cca8ac8d960d532b
marcosviniciusjau <marcosviniciusjahu@gmail.com>
2022-12-14 15:17:17 -0300

    Adicionar arquivos de projeto.

---
+++ AppGuiaDev.Android/SpalshScreen.cs
@@ -0,0 +1,25 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AppGuiaDev.Droid
+{
+    [Activity(Label = "SpalshScreen", MainLauncher = true, NoHistory = true)]
+    public class SpalshScreen : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            StartActivity(new Intent(Aplication.Context, typeof(MainActivity)))
+
+            // Create your application here
+        }
+    }
+}
---
+++ AppGuiaDev/MainPage.xaml.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+using AppGuiaDev.View;
+
+namespace AppGuiaDev
+{
+    public partial class MainPage : MasterDetailPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            Detail = new NavigationPage (new Inicial());
+        }
+
+        private void Button_Clicked_Contato(object sender, EventArgs e)
+        {
+            Detail = new NavigationPage(new Contato());
+            IsPresented = false;
+        }
+
+        private void Button_Clicked_BancodeDados(object sender, EventArgs e)
+        {
+            Detail = new NavigationPage(new BancodeDados());
+            IsPresented = false;
+        }
+
+        private void Button_Clicked_DesignGrafico(object sender, EventArgs e)
+        {
+            Detail = new NavigationPage(new DesignGrafico());
+            IsPresented = false;
+        }
+
+        private void Button_Clicked_Inicial(object sender, EventArgs e)
+        {
+            Detail = new NavigationPage(new Inicial());
+            IsPresented = false;
+        }
+
+        private void Button_Clicked_ProgramacaoWeb(object sender, EventArgs e)
+        {
+            Detail = new NavigationPage(new ProgramacaoWeb());
+            IsPresented = false;
+        }
+
+        private void Button_Clicked_ProgramacaoMobile(object sender, EventArgs e)
+        {
+            Detail = new NavigationPage(new ProgramacaoMobile());
+            IsPresented = false;
+        }
+
+        private void Button_Clicked_RedesdeComputadores(object sender, EventArgs e)
+        {
+            Detail = new NavigationPage(new RedesdeComputadores());
+            IsPresented = false;
+        }
+
+        private void Button_Clicked_SegurancadaInformacao(object sender, EventArgs e)
+        {
+            Detail = new NavigationPage(new SegurancadaInformacao());
+            IsPresented = false;
+        }
+
+        private void Button_Clicked_SistemasEmbarcados(object sender, EventArgs e)
+        {
+            Detail = new NavigationPage(new SistemasEmbarcados());
+            IsPresented = false;
+        }
+
+        private void Button_Clicked_SistemasOperacionais(object sender, EventArgs e)
+        {
+            Detail = new NavigationPage(new SistemasOperacionais());
+            IsPresented = false;
+        }
+
+        private void Button_Clicked_Sobre(object sender, EventArgs e)
+        {
+            Detail = new NavigationPage(new Sobre());
+            IsPresented = false;
+        }
+
+       
+    }
+}
